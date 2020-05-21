8d32150de8411795cfd887856e7585470392baa9
evreke <evreke@gmail.com>
2020-05-21 17:29:04 +0300

    Модель пользователя

---
+++ src/main/kotlin/ru/evreke/demo/entity/User.kt
@@ -0,0 +1,17 @@
+package ru.evreke.demo.entity
+
+import java.time.LocalDateTime
+import javax.persistence.*
+
+@Entity
+@Table(name = "users")
+data class User(
+    @Column(unique = true)
+    var username: String,
+    var phoneNumber: String
+) {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long? = null
+    val createdAt: LocalDateTime = LocalDateTime.now()
+}
