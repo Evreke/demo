ede4d4ac5c653d4dce6c9cadd846f5332a531407
evreke <evreke@gmail.com>
2020-05-22 18:40:22 +0300

    Коммит 99e229f7 позволил отказаться от костыля в видео MovieSessionDto.

--- src/main/kotlin/ru/evreke/demo/controllers/api/v1/UserApi.kt
+++ src/main/kotlin/ru/evreke/demo/controllers/api/v1/UserApi.kt
@@ -2,7 +2,7 @@ package ru.evreke.demo.controllers.api.v1
 
 import org.springframework.web.bind.annotation.*
 import ru.evreke.demo.entity.Booking
-import ru.evreke.demo.entity.MovieSessionDto
+import ru.evreke.demo.entity.MovieSession
 import ru.evreke.demo.repository.BookingRepository
 import ru.evreke.demo.repository.MovieSessionRepository
 
@@ -22,7 +22,7 @@ class UserApi(
     @GetMapping("/{id}/movie-sessions")
     fun getAllUserSessions(
         @PathVariable id: Long
-    ): List<MovieSessionDto> {
+    ): List<MovieSession> {
         return movieSessionRepo.findSessionsByUserId(id)
     }
 }
--- src/main/kotlin/ru/evreke/demo/repository/MovieSessionRepository.kt
+++ src/main/kotlin/ru/evreke/demo/repository/MovieSessionRepository.kt
@@ -3,9 +3,8 @@ package ru.evreke.demo.repository
 import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.CrudRepository
 import ru.evreke.demo.entity.MovieSession
-import ru.evreke.demo.entity.MovieSessionDto
 
 interface MovieSessionRepository : CrudRepository<MovieSession, Long> {
-    @Query(value = "select new ru.evreke.demo.entity.MovieSessionDto(ms.id, ms.date, ms.startedAt, ms.endedAt, ms.movie.id) from MovieSession as ms inner join ms.users as u where u.id = ?1")
-    fun findSessionsForUser(id: Long): List<MovieSessionDto>
+    @Query(value = "select ms from MovieSession as ms inner join User as u on u.id = ?1")
+    fun findSessionsByUserId(id: Long): List<MovieSession>
 }
