6f11dd854187f91f367d19bbe63e0ec2732e249b
evreke <evreke@gmail.com>
2020-05-22 10:59:24 +0300

    Базовый rest контроллер для работы с сущностью Movie

---
+++ src/main/kotlin/ru/evreke/demo/controllers/api/v1/MovieApi.kt
@@ -0,0 +1,52 @@
+package ru.evreke.demo.controllers.api.v1
+
+import org.springframework.web.bind.annotation.*
+import ru.evreke.demo.entity.Movie
+import ru.evreke.demo.exceptions.NotFoundException
+import ru.evreke.demo.repository.MovieRepository
+
+@RestController
+@RequestMapping("/api/v1/movies")
+class MovieApi(
+    private val repo: MovieRepository
+) {
+    @GetMapping("/", "")
+    fun getAllMovies(): MutableIterable<Movie> {
+        return repo.findAll()
+    }
+
+    @GetMapping("/{id}")
+    fun getMovie(
+        @PathVariable id: Long
+    ): Movie? {
+        return repo.findById(id).orElseThrow { NotFoundException("Movie with id=$id not found") }
+    }
+
+    @PostMapping("/", "")
+    fun createMovie(
+        @RequestBody movie: Movie
+    ) {
+        repo.save(movie)
+    }
+
+    @PutMapping("/{id}")
+    fun editMovie(
+        @PathVariable id: Long,
+        @RequestBody movie: Movie
+    ) {
+        repo.save(
+            repo.findById(id).orElseThrow { NotFoundException("Movie with id=$id not found") }.also {
+                it.duration = movie.duration
+                it.title = movie.title
+            }
+        )
+    }
+
+    @DeleteMapping("/{id}")
+    fun deleteMovie(
+        @PathVariable id: Long
+    ) {
+        repo.deleteById(id)
+    }
+
+}
