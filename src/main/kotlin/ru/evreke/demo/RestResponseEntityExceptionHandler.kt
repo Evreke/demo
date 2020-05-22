0c8053cc48f6efc9dce8d5684f9c976edd11ef10
evreke <evreke@gmail.com>
2020-05-22 10:58:02 +0300

    Универсальный обработчик исключений. Добавлен обработчик исключений типа NotFoundException.

---
+++ src/main/kotlin/ru/evreke/demo/RestResponseEntityExceptionHandler.kt
@@ -0,0 +1,20 @@
+package ru.evreke.demo
+
+import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.ControllerAdvice
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.context.request.WebRequest
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler
+import ru.evreke.demo.exceptions.NotFoundException
+
+@ControllerAdvice
+class RestResponseEntityExceptionHandler : ResponseEntityExceptionHandler() {
+
+    @ExceptionHandler(value = [NotFoundException::class])
+    fun handleNotFound(ex: Exception, request: WebRequest): ResponseEntity<Any> {
+        val responseBody = ex.message
+        return handleExceptionInternal(ex, responseBody, HttpHeaders(), HttpStatus.NOT_FOUND, request)
+    }
+}
