a14fa3a5a1e9c363a88e1da1bb90fccbfc2432e5
evreke <evreke@gmail.com>
2020-05-24 14:18:16 +0300

    Возвращает список сеансов для которых бронь была оплачена.

--- src/main/kotlin/ru/evreke/demo/repository/MovieSessionRepository.kt
+++ src/main/kotlin/ru/evreke/demo/repository/MovieSessionRepository.kt
@@ -5,6 +5,10 @@ import org.springframework.data.repository.CrudRepository
 import ru.evreke.demo.entity.MovieSession
 
 interface MovieSessionRepository : CrudRepository<MovieSession, Long> {
-    @Query(value = "select ms from MovieSession as ms inner join User as u on u.id = ?1")
+    @Query(value = "SELECT * " +
+        "FROM movie_sessions as ms " +
+        "JOIN users_movie_sessions ums on ms.id = ums.movie_session_id " +
+        "JOIN bookings as b on b.movie_session_id = ums.movie_session_id " +
+        "WHERE b.payed = true and ums.user_id = ?1", nativeQuery = true)
     fun findSessionsByUserId(id: Long): List<MovieSession>
 }
