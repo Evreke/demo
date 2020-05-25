621646b265b12bad421e8913a5f6b772d819df11
evreke <evreke@gmail.com>
2020-05-25 16:15:23 +0300

    Тестовые данные учитывают изменения в коммитах 7fb10886, ae7e1833

--- src/main/resources/data-h2.sql
+++ src/main/resources/data-h2.sql
@@ -9,29 +9,19 @@ values ('Клиент'),
 
 insert into categories (title, discount)
 values ('Обычная', 0),
-       ('Социальная-1', 10),
-       ('Социальная-2', 20),
-       ('Социальная-3', 30);
+       ('Социальная-1', 0.1),
+       ('Социальная-2', 0.2),
+       ('Социальная-3', 0.3);
 
 insert into halls(name, capacity)
 values ('Зелёный', 200),
        ('VIP', 10);
 
-insert into movie_sessions (started_at, ended_at, date, movie_id, hall_id)
-values ('10:00', '12:00', '2020-02-22', 1, 1),
-       ('13:00', '14:30', '2020-02-22', 2, 1),
-       ('15:00', '17:30', '2020-02-22', 3, 2);
+insert into movie_sessions (started_at, ended_at, date, movie_id, hall_id, price)
+values ('10:00', '12:00', '2020-02-22', 1, 1, 400),
+       ('13:00', '14:30', '2020-02-22', 2, 1, 500),
+       ('15:00', '17:30', '2020-02-22', 3, 2, 450);
 
 insert into users (username, role_id, category_id)
 values ('Вася', 1, 2),
        ('Петя', 1, 1);
-
-insert into users_movie_sessions
-values (1, 1),
-       (1, 2),
-       (2, 3);
-
-insert into bookings (movie_session_id, user_id, payed)
-values (2, 1, false),
-       (1, 1, true),
-       (3, 2, true);
