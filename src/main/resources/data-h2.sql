b59d274537181b6647ebb2aac3f533a3703e211b
evreke <evreke@gmail.com>
2020-05-25 21:58:57 +0300

    Тестовые данные были изменены в соответсвии с правками. Так же было подобрано время для movie_sessions, чтобы отражать приближенную к реальности ситуацию.

--- src/main/resources/data-h2.sql
+++ src/main/resources/data-h2.sql
@@ -17,10 +17,10 @@ insert into halls(name, capacity)
 values ('Зелёный', 200),
        ('VIP', 10);
 
-insert into movie_sessions (started_at, ended_at, date, movie_id, hall_id, price)
-values ('10:00', '12:00', '2020-02-22', 1, 1, 400),
-       ('13:00', '14:30', '2020-02-22', 2, 1, 500),
-       ('15:00', '17:30', '2020-02-22', 3, 2, 450);
+insert into movie_sessions (started_at, ended_at, start_selling_at, movie_id, hall_id, price)
+values (dateadd('hour', 3, now()), dateadd('minute', 180+90, now()), dateadd('hour', 2, now()), 1, 1, 400),
+       (dateadd('hour', 3, now()), dateadd('minute', 180+120, now()), dateadd('hour', 2, now()), 2, 1, 500),
+       (dateadd('hour', 3, now()), dateadd('minute', 180+100, now()), dateadd('hour', 2, now()), 3, 2, 450);
 
 insert into users (username, role_id, category_id)
 values ('Вася', 1, 2),
