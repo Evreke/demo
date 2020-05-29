288818a3f6f2fd842a224c158ff7227b057cddf3
evreke <evreke@gmail.com>
2020-05-29 14:28:45 +0300

    Тесты теперь используют моки

--- src/test/kotlin/ru/evreke/demo/services/BookingServiceImplTest.kt
+++ src/test/kotlin/ru/evreke/demo/services/BookingServiceImplTest.kt
@@ -3,12 +3,15 @@ package ru.evreke.demo.services
 import com.github.javafaker.Faker
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
+import org.mockito.Mockito
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.test.context.TestPropertySource
 import ru.evreke.demo.entity.*
 import ru.evreke.demo.exceptions.AlreadyPayedException
 import ru.evreke.demo.exceptions.BookingException
+import ru.evreke.demo.repository.BookingRepository
 import java.math.BigDecimal
 import java.time.LocalDateTime
 import java.time.LocalTime
@@ -20,16 +23,13 @@ internal class BookingServiceImplTest {
 
     @Autowired
     private lateinit var bookingService: BookingServiceImpl
+
+    @MockBean
+    private lateinit var bookingRepo: BookingRepository
     private val faker = Faker()
     private val time: LocalDateTime = LocalDateTime.now()
     private val movie = Movie(title = faker.artist().name(), duration = LocalTime.of(1, 30))
     private val hall = Hall(name = faker.elderScrolls().firstName(), capacity = Random.nextInt(10, 200))
-    private val privilegedMovieSession = MovieSession(startedAt = time, endedAt = time.plusMinutes(90)).also {
-        it.hall = hall
-        it.movie = movie
-        it.privileged = true
-        it.price = BigDecimal("500")
-    }
     private val privilegedMovieSessionWithOpenedSaleForNonPrivilegedUsers = MovieSession(startedAt = time, endedAt = time.plusMinutes(90)).also {
         it.hall = hall
         it.movie = movie
@@ -58,11 +58,12 @@ internal class BookingServiceImplTest {
 
     @Test
     fun createBooking() {
-        val booking = Booking(session = privilegedMovieSession).also {
+        val booking = Booking(session = privilegedMovieSessionWithOpenedSaleForNonPrivilegedUsers).also {
             it.session.booked++
             it.user = nonPrivilegedUser
-            it.totalPrice = bookingService.evaluateTotalPrice(nonPrivilegedUser, privilegedMovieSession)
+            it.totalPrice = bookingService.evaluateTotalPrice(nonPrivilegedUser, privilegedMovieSessionWithOpenedSaleForNonPrivilegedUsers)
         }
+        Mockito.`when`(bookingRepo.save(Mockito.any(Booking::class.java))).thenReturn(booking)
         val result = bookingService.createBooking(privilegedMovieSessionWithOpenedSaleForNonPrivilegedUsers, nonPrivilegedUser)
         Assertions.assertNotNull(result)
         Assertions.assertEquals(booking.user, result.user)
@@ -77,51 +78,85 @@ internal class BookingServiceImplTest {
 
     @Test
     fun createBookingForPrivilegedUserWithClosedSale() {
+        val booking = Booking(session = privilegedMovieSessionWithClosedSaleForNotPrivilegedUsers).also {
+            it.session.booked++
+            it.user = privilegedUser
+            it.totalPrice = bookingService.evaluateTotalPrice(privilegedUser, privilegedMovieSessionWithClosedSaleForNotPrivilegedUsers)
+        }
+        Mockito.`when`(bookingRepo.save(Mockito.any(Booking::class.java))).thenReturn(booking)
         Assertions.assertNotNull(bookingService.createBooking(movieSession = privilegedMovieSessionWithClosedSaleForNotPrivilegedUsers, user = privilegedUser))
     }
 
     @Test
     fun createBookingForNotPrivilegedUserWithClosedSaleButWithNotPrivilegedSession() {
-        val booking = Booking(session = privilegedMovieSession).also {
+        val booking = Booking(session = notPrivilegedMovieSessionWithClosedSaleForNotPrivilegedUsers).also {
             it.session.booked++
             it.user = nonPrivilegedUser
-            it.totalPrice = bookingService.evaluateTotalPrice(nonPrivilegedUser, privilegedMovieSession)
+            it.totalPrice = bookingService.evaluateTotalPrice(nonPrivilegedUser, notPrivilegedMovieSessionWithClosedSaleForNotPrivilegedUsers)
         }
+        Mockito.`when`(bookingRepo.save(Mockito.any(Booking::class.java))).thenReturn(booking)
         val result = bookingService.createBooking(movieSession = notPrivilegedMovieSessionWithClosedSaleForNotPrivilegedUsers, user = nonPrivilegedUser)
         Assertions.assertEquals(booking.user, result.user)
     }
 
     @Test
     fun evaluateDiscountForNotPrivilegedUser() {
+        val booking = Booking(session = notPrivilegedMovieSessionWithClosedSaleForNotPrivilegedUsers).also {
+            it.session.booked++
+            it.user = nonPrivilegedUser
+            it.totalPrice = bookingService.evaluateTotalPrice(nonPrivilegedUser, notPrivilegedMovieSessionWithClosedSaleForNotPrivilegedUsers)
+        }
+        Mockito.`when`(bookingRepo.save(Mockito.any(Booking::class.java))).thenReturn(booking)
         val resultWithoutDiscount = bookingService.createBooking(privilegedMovieSessionWithOpenedSaleForNonPrivilegedUsers, nonPrivilegedUser)
         Assertions.assertEquals(resultWithoutDiscount.totalPrice, bookingService.evaluateTotalPrice(nonPrivilegedUser, privilegedMovieSessionWithOpenedSaleForNonPrivilegedUsers))
     }
 
     @Test
     fun evaluateDiscountForPrivilegedUser() {
+        val booking = Booking(session = privilegedMovieSessionWithOpenedSaleForNonPrivilegedUsers).also {
+            it.session.booked++
+            it.user = privilegedUser
+            it.totalPrice = bookingService.evaluateTotalPrice(privilegedUser, privilegedMovieSessionWithOpenedSaleForNonPrivilegedUsers)
+        }
+        Mockito.`when`(bookingRepo.save(Mockito.any(Booking::class.java))).thenReturn(booking)
         val resultWithDiscount = bookingService.createBooking(privilegedMovieSessionWithOpenedSaleForNonPrivilegedUsers, privilegedUser)
         Assertions.assertEquals(resultWithDiscount.totalPrice, bookingService.evaluateTotalPrice(privilegedUser, privilegedMovieSessionWithOpenedSaleForNonPrivilegedUsers))
     }
 
     @Test
     fun deletePayedBooking() {
-        val booking = bookingService.createBooking(privilegedMovieSessionWithOpenedSaleForNonPrivilegedUsers, nonPrivilegedUser)
-        bookingService.payBooking(booking.id!!)
-        Assertions.assertThrows(AlreadyPayedException::class.java) { bookingService.deleteBooking(booking.id!!, booking.user?.id!!) }
+        val booking = Booking(session = privilegedMovieSessionWithOpenedSaleForNonPrivilegedUsers).also {
+            it.session.booked++
+            it.user = nonPrivilegedUser
+            it.totalPrice = bookingService.evaluateTotalPrice(nonPrivilegedUser, privilegedMovieSessionWithOpenedSaleForNonPrivilegedUsers)
+        }
+        Mockito.`when`(bookingRepo.save(Mockito.any(Booking::class.java))).thenReturn(booking)
+        bookingService.payBooking(booking)
+        Assertions.assertThrows(AlreadyPayedException::class.java) { bookingService.deleteBooking(booking, booking.user) }
     }
 
     @Test
     fun payBooking() {
-        val booking = bookingService.createBooking(privilegedMovieSessionWithOpenedSaleForNonPrivilegedUsers, nonPrivilegedUser)
-        bookingService.payBooking(booking.id!!)
-        val isPayed = bookingService.getBooking(booking.id!!).payed
+        val booking = Booking(session = privilegedMovieSessionWithOpenedSaleForNonPrivilegedUsers).also {
+            it.session.booked++
+            it.user = nonPrivilegedUser
+            it.totalPrice = bookingService.evaluateTotalPrice(nonPrivilegedUser, privilegedMovieSessionWithOpenedSaleForNonPrivilegedUsers)
+        }
+        Mockito.`when`(bookingRepo.save(Mockito.any(Booking::class.java))).thenReturn(booking)
+        bookingService.payBooking(booking)
+        val isPayed = booking.payed
         Assertions.assertTrue(isPayed)
     }
 
     @Test
     fun payPayedBooking() {
-        val booking = bookingService.createBooking(privilegedMovieSessionWithOpenedSaleForNonPrivilegedUsers, nonPrivilegedUser)
-        bookingService.payBooking(booking.id!!)
-        Assertions.assertThrows(AlreadyPayedException::class.java) { bookingService.payBooking(booking.id!!) }
+        val booking = Booking(session = privilegedMovieSessionWithOpenedSaleForNonPrivilegedUsers).also {
+            it.session.booked++
+            it.user = nonPrivilegedUser
+            it.totalPrice = bookingService.evaluateTotalPrice(nonPrivilegedUser, privilegedMovieSessionWithOpenedSaleForNonPrivilegedUsers)
+        }
+        Mockito.`when`(bookingRepo.save(Mockito.any(Booking::class.java))).thenReturn(booking)
+        bookingService.payBooking(booking)
+        Assertions.assertThrows(AlreadyPayedException::class.java) { bookingService.payBooking(booking) }
     }
 }
