a1e66e1d7e1aa40c99356d6eb1f307c50bcffe50
evreke <evreke@gmail.com>
2020-05-29 12:10:30 +0300

    Добавлена конфигурация для тестов. Тестирование производится в отдельной базе h2. Добавлена библиотека Faker для заполнения тестовых полей случайными данными (зависимость можно убрать, но тогда перепишите соответствующие места в тестах)

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,6 +21,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.flywaydb:flyway-core")
+    implementation("com.github.javafaker:javafaker:1.0.2")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     runtimeOnly("com.h2database:h2")
